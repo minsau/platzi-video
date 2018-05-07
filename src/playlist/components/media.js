67849a0a295970ddb8a36a8a65f06643605eca59
ROMERO MUÑOZ <s1881085@mexeo.bns>
2018-05-07 18:04:46 -0500

    Agregado src

---
+++ src/playlist/components/media.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Media extends React.Component {
+    render(){
+        return (
+            <div>
+                <div>
+                    <img src="" alt="" width={260} height={160} />
+                </div>
+                <p>Hola mundo!</p>
+            </div>
+        );
+    }
+}
