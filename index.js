1c9e765373247412fa8e5a4919bd7fef35a00fa3
Saúl Gómez Navarrete <minsau2@gmail.com>
2018-05-07 23:36:23 -0500

    Video 10

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import  React  from 'react';
 import ReactDOM from 'react-dom';
+import Media from './src/playlist/components/media';
 
 const appContainer = document.getElementById('app');
-const holaMundo = <h1>Hola Mundo! Modified</h1>;
-ReactDOM.render(holaMundo, appContainer);
+// const holaMundo = <h1>Hola Mundo! Modified</h1>;
+ReactDOM.render(<Media 
+    type="video"
+    image="./images/covers/bitcoin.jpg"
+    title="¿Que es un bitcoin?"
+    author="Saúl Gómez Navarrete" />, appContainer);
--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,15 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import './media.css';
+import PropTypes from 'prop-types';
 
-class Media extends React.Component {
+class Media extends Component {
     render(){
+        const styles = {
+            container: {
+                color: '#44546b',
+                cursor: 'pointer',
+                width: 260,
+                border: '1px solid black'
+            }
+        };
         return (
-            <div>
-                <div>
-                    <img src="" alt="" width={260} height={160} />
+            <div className="Media">
+                <div className="Media-cover">
+                    <img src={this.props.image} 
+                        className="Media-image"
+                        alt="" width={260} height={160} />
                 </div>
-                <p>Hola mundo!</p>
+                <h3 className="Media-title">{this.props.title}</h3>
+                <p className="Media-author">{this.props.author}</p>
             </div>
         );
     }
 }
+
+Media.propTypes = {
+    image: PropTypes.string,
+    author: PropTypes.string,
+    title: PropTypes.string,
+    type: PropTypes.oneOf(['video', 'audio']),
+};
+
+export default Media;
